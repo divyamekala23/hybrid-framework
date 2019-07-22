fd38b8b50e071d49780281060fb521b4d75c11b5
Divya Mekala <dvpadala@gmail.com>
2019-07-22 00:27:22 -0400

    Add file reader

---
+++ src/main/java/com/divya/fileutils/FileReader.java
@@ -0,0 +1,9 @@
+package com.divya.fileutils;
+
+/**
+ * This class will read a text input file and stores them in an array of UserCredentials.
+ * input will be file name
+ * output will be
+ */
+public class FileReader {
+}
