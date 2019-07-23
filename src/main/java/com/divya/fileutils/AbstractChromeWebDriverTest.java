004d8cc6d2a5c58f941c3da6c30bda7c89c0be28
Divya Mekala <dvpadala@gmail.com>
2019-07-22 23:08:19 -0400

    added Abstract super class

---
+++ src/main/java/com/divya/fileutils/AbstractChromeWebDriverTest.java
@@ -0,0 +1,46 @@
+package com.divya.fileutils;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class AbstractChromeWebDriverTest {
+
+    protected WebDriver driver;
+
+    public AbstractChromeWebDriverTest() {
+        super();
+    }
+
+    @BeforeTest
+    public void beforeTest() {
+        // download the webdriver executable
+
+        WebDriverManager.chromedriver().setup();
+
+        // create a instance of your web driver
+
+        driver = new ChromeDriver();
+    }
+
+    @AfterTest
+    public void afterTest() {
+
+        driver.quit();
+    }
+
+    public void sleep(int seconds) {
+
+        try {
+            Thread.sleep(seconds * 1000);
+
+        } catch (InterruptedException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+}
--- src/main/java/com/divya/fileutils/FileReader.java
+++ src/main/java/com/divya/fileutils/FileReader.java
@@ -9,5 +9,6 @@ public class FileReader {
 
     public static void main(String[] args) {
 
+
     }
 }
